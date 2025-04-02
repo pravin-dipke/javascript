d6d03b72eb74a01030fefe89d7657efce2c7e903
pravin-dipke <pravinbdipke@gmail.com>
2025-04-02 11:06:32 +0530

    dataTypes in javascript

---
+++ 01_Basics/02_dataTypes.js
@@ -0,0 +1,19 @@
+let name = "pravin"
+let age = "18"
+let isUserLoggedIn = true
+
+
+/*
+    -- more dataTypes
+     - number => 3
+     - string => ""
+     - bigint => for long intergers
+     - boolean => true/false
+     - null => standalone value
+     - undefined => 
+     - symbol => unique
+
+*/
+
+console.log(typeof null) // object
+console.log(typeof undefined) // undefined
