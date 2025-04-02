f8b1ada2a8bac5fd6fd0536e22521ebf7034320e
pravin-dipke <pravinbdipke@gmail.com>
2025-04-02 19:47:23 +0530

    dataType conversion in javascript

---
+++ 01_Basics/03_conversionOperation.js
@@ -0,0 +1,20 @@
+let score = "42asd"
+
+// console.log(typeof score)
+// console.log(typeof(score))
+
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber)
+
+let isLoggedIn = 1
+
+let booleanConversion = Boolean(isLoggedIn)
+// console.log(booleanConversion)
+
+// Number to string conversion
+let someNumber = 48
+
+let stringNumber = String(someNumber)
+console.log(typeof stringNumber)
+console.log(stringNumber)
